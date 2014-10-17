727325756a268beccbca6a53db52cc267d0a9993
eacharya <eacharya@gmail.com>
2014-10-16 17:11:13 -0700

    Issue 23: Annotate new images that don't have associated annotation files already created

--- java/video_annotation/src/com/zigvu/video/model/AnnotationModel.java
+++ java/video_annotation/src/com/zigvu/video/model/AnnotationModel.java
@@ -33,6 +33,36 @@ public class AnnotationModel {
 			annotationFiles = new ArrayList<String>();
 			File[] annoFiles = (new File(commandLine.annotationsFolder))
 					.listFiles();
+			File[] frameFiles = (new File(commandLine.framesFolder))
+					.listFiles();
+
+			// create empty annotations for frames that don't currently have
+			// them
+			for (int j = 0; j < frameFiles.length; j++) {
+				File frameFile = frameFiles[j];
+				if (frameFile.isFile() && frameFile.getName().endsWith(".png")) {
+					boolean frameHasNoAnnotation = true;
+					AnnotationReaderWriter annotationRW = newAnnotationRW(frameFile
+							.getName());
+					for (int i = 0; i < annoFiles.length; i++) {
+						if (annoFiles[i].getName().equalsIgnoreCase(
+								annotationRW.getAnnotationFileName())) {
+							frameHasNoAnnotation = false;
+							break;
+						}
+					}
+					if (frameHasNoAnnotation) {
+						Annotator.log(Annotator.logDebug,
+								"AnnotationModel: Saving new annotation: "
+										+ annotationRW.getAnnotationFileName());
+						annotationRW.write(null);
+					}
+				}
+			}
+			// reread annotations
+			annoFiles = (new File(commandLine.annotationsFolder)).listFiles();
+
+			// sort and organize annotations
 			String[] annoFilesStr = new String[annoFiles.length];
 			for (int i = 0; i < annoFiles.length; i++) {
 				annoFilesStr[i] = annoFiles[i].getAbsolutePath();
@@ -47,6 +77,7 @@ public class AnnotationModel {
 			Annotator.log(Annotator.logInfo,
 					"AnnotationModel: Num of annotation files read: "
 							+ annotationFiles.size());
+
 			setCurrentAnno(0);
 		}
 	}
@@ -89,13 +120,15 @@ public class AnnotationModel {
 		try {
 			currentFrame = ImageIO.read(imageFile);
 		} catch (IOException e) {
-			Annotator.log(Annotator.logError,
+			Annotator.log(
+					Annotator.logError,
 					"AnnotationModel: Error reading image file: "
-					+ imageFile.getPath());
+							+ imageFile.getPath());
 			e.printStackTrace();
 		}
 	}
 
+	// new annotation reader for video annotation
 	public AnnotationReaderWriter newAnnotationRW(long timeIndex) {
 		String annotationFileName = commandLine.videoFilePrefix() + "_"
 				+ timeIndex + ".json";
@@ -104,6 +137,32 @@ public class AnnotationModel {
 		AnnotationReaderWriter annotationRW = new AnnotationReaderWriter();
 		annotationRW.setDimension(mediaInfo.getWidth(), mediaInfo.getHeight());
 		annotationRW.setTime(timeIndex);
+		annotationRW.setFileNames(annotationFileName, frameFileName);
+		annotationRW.setFolderNames(commandLine.annotationsFolder,
+				commandLine.framesFolder);
+		return annotationRW;
+	}
+
+	// new annotation reader for image annotation
+	public AnnotationReaderWriter newAnnotationRW(String frameFileName) {
+		File frameFile = new File(commandLine.framesFolder, frameFileName);
+		BufferedImage frame = null;
+		try {
+			frame = ImageIO.read(frameFile);
+		} catch (IOException e) {
+			Annotator.log(
+					Annotator.logError,
+					"AnnotationModel: Error reading image file: "
+							+ frameFile.getPath());
+			e.printStackTrace();
+		}
+		String annotationFileName = frameFile.getName().substring(0,
+				frameFile.getName().lastIndexOf("."))
+				+ ".json";
+
+		AnnotationReaderWriter annotationRW = new AnnotationReaderWriter();
+		annotationRW.setDimension(frame.getWidth(), frame.getHeight());
+		annotationRW.setTime(0);
 		annotationRW.setFileNames(annotationFileName, frameFileName);
 		annotationRW.setFolderNames(commandLine.annotationsFolder,
 				commandLine.framesFolder);
